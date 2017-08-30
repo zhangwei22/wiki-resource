24e6fa1a40fe9a3aa170e579e0c84707d357a15a
zhangwei <zhangwei@mimidai.com>
2017-08-30 16:32:52 +0800

    完成项目在tomcat中部署

--- src/main/java/com/github/wiki/Application.java
+++ src/main/java/com/github/wiki/Application.java
@@ -2,11 +2,26 @@ package com.github.wiki;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-public class Application {
+@SpringBootApplication(scanBasePackages = "com.github.wiki")
+@RestController
+public class Application extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    @RequestMapping("/")
+    public String greeting() {
+        return "Hello World!";
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 }
